a003b2722b83639f16a11d5f8236f02418a9d230
Shamuskado22 <samucaoliveira1995@gmail.com>
2024-11-19 19:38:32 -0300

    feat: adicionado dados testes para posts, função de busca por id e requisição de post por id

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 import express from "express";
 
+const posts = [
+  { 
+    id: 1,
+    descricao: "Uma foto teste",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Gato fofo dormindo",
+    imagem: "https://placekitten.com/400/200",
+  },
+  {
+    id: 3,
+    descricao: "Paisagem incrível",
+    imagem: "https://source.unsplash.com/random",
+  },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-  req.statusCode(200).send("Boas vindas à imersão");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
 });
 
-console.log(req)
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id)
+  })
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id)
+  res.status(200).json(posts[index]);
+})
